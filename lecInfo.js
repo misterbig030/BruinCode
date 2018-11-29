2a585e8603ffc58c98134f46f3254975d4d61956
arian96 <arian.deraakhshanfar@gmail.com>
2018-11-29 09:14:11 -0800

    Update lecInfo.js

--- lecInfo.js
+++ lecInfo.js
@@ -1,27 +1,37 @@
-$( document ).ready(function()
+var datas;
+var keys;
+var count = 0;
+var k;
+var foundItem;
+var ob;
+var className;
+var profName;
+var courseArr=[];
+var usedClasses=[];
+
+ $( document ).ready(function()
 {
     var database;
     var config = {
-       apiKey: "AIzaSyC_7hQrJC_OrgmlugFQdJhipXnaG8D1xD8",
-       authDomain: "bruincode.firebaseapp.com",
-       databaseURL: "https://bruincode.firebaseio.com",
-       projectId: "bruincode",
-       storageBucket: "bruincode.appspot.com",
-       messagingSenderId: "461364672091"
-     };
-     firebase.initializeApp(config);
-
+     apiKey: "AIzaSyC_7hQrJC_OrgmlugFQdJhipXnaG8D1xD8",
+     authDomain: "bruincode.firebaseapp.com",
+     databaseURL: "https://bruincode.firebaseio.com",
+     projectId: "bruincode",
+     storageBucket: "bruincode.appspot.com",
+     messagingSenderId: "461364672091"
+   };
+    firebase.initializeApp(config);
     database = firebase.database();
     var ref=database.ref('courseData');
 
-    var courseArr=[];
-    ref.on('value',gotData);     //get data from firebase
+    ref.on('value', gotData);     //get data from firebase
+
     function gotData(data){
-      //console.log(data.val());
+
       var datas = data.val();
       var keys = Object.keys(datas);
 
-      for(var i=0;i< keys.length;i++)
+      for(var i=0; i< keys.length; i++)
       {
         var k=keys[i];
         var cNum = datas[k].courseNumber;
@@ -32,6 +42,12 @@ $( document ).ready(function()
         var cOutHr=datas[k].outHours;
         var cSession=datas[k].session;
         var cUnits=datas[k].units;
+        var cLecNum=datas[k].lecNum;
+        var cProj=datas[k].projects;
+        var cEssay=datas[k].essays;
+        var cSyll=datas[k].syllabus;
+        var cRead=datas[k].readWeek;
+
         var ob={courseNumber:cNum,
                 session:cSession,
                 units:cUnits,
@@ -39,61 +55,116 @@ $( document ).ready(function()
                 professor:cPro,
                 lecHours:cLecHr,
                 disHours:cDisHr,
-                outHours:cOutHr};
-        //console.log(ob);
+                outHours:cOutHr,
+                lecNum:cLecNum,
+                projects:cProj,
+                essays:cEssay,
+                syllabus:cSyll,
+                readWeek:cRead
+              };
+
         courseArr.push(ob);
       }
-      console.log(courseArr);
+        console.log(courseArr.length);
 
-      var className= document.getElementById("cName");
-      var claNam=localStorage.getItem("classNam");
-      className.innerHTML="Class Name: "+claNam;
+      className= document.getElementById("cName");
+      className.innerHTML=localStorage.getItem("classNam");
 
-      //var profName= document.getElementById("pName");
-      var profNam =localStorage.getItem("profNam");
-      //className.innerHTML= profNam;
+      profName= document.getElementById("pName");
+      profName.innerHTML=localStorage.getItem("profNam");
 
-      var objClass=[];
-      for(var i=0;i<courseArr.length;i++)
+      for(foundItem=0; foundItem< courseArr.length; foundItem++)
       {
-
-            if(claNam==courseArr[i].courseNumber && profNam=="Professor "+courseArr[i].professor)
-            {
-              objClass.push(courseArr[i]);
-              //showClasInfo(courseArr,i,session);
-            }
-
+        if(className.innerHTML == courseArr[foundItem].courseNumber)
+        {
+          showWorkLoad();
+          break;
+        }
       }
 
+  }
 
-      var table = document.getElementById("myTable");
-      var row1 = table.insertRow(0);
-      var cell11 = row1.insertCell(0);
-      var cell12 = row1.insertCell(1);
+});
 
-      cell11.innerHTML=objClass[0].professor;
-      cell12.innerHTML=objClass[0].session;
-    //  cell11.setAttribute("style","font-size:30px;font-weight:bold");
 
-   var row2 = table.insertRow(1);
-      var cell21 = row2.insertCell(0);
-      var cell22 = row2.insertCell(1);
-      //cell21.innerHTML="professor: "+arr[index].professor;
+  function addToSchedule()
+  {
+     alert(localStorage.classNam);
+    var database = firebase.database();
+    var refToSch=database.ref('scheduleTrack');
 
-      var button = document.createElement("Button");
-      button.type="button";
-      button.class="btn";
-      button.innerHTML="Compare with Others";
-      cell21.appendChild(button);
-      button.setAttribute("style","height:30px;width:150px;background-color:#CEC973");
-      cell22.innerHTML="Inclass time per week: "+(parseFloat(objClass[0].lecHours)+parseFloat(objClass[0].disHours))+" Hrs";
-  /*
-      button.onclick = function()
-        {
+    refToSch.push(courseArr[foundItem]);
+    // refToSch.remove();
+  }
 
-          window.location.href="lecInfo.html";
-        }
-*/
+  function moveToSchedule()
+  {
+    window.location.href="schedulePage.html";
+  }
+
+
+  function compareClasses() {
+    window.location.href="188 Comparison.html";
+  }
+
+
+function showWorkLoad() {
+  var listDiv = document.getElementById("lists");
+  var myList = document.createElement("UL");
+  listDiv.appendChild(myList);
+
+  for (var i = 0; i < 6; i++) {
+      // create an item for each one
+      var listItem = document.createElement('li');
+      //listItem.style.align("center");
+
+      if (i==0)
+      {
+        // Add the item text
+        listItem.innerHTML = "Total Lecture Hours: " + courseArr[foundItem].lecHours;
+      }
+      else if(i==1)
+      {
+        // Add the item text
+        listItem.innerHTML = "Total Discussion Hours: " + courseArr[foundItem].disHours;
+      }
+      else if(i==2)
+      {
+        // Add the item text
+        listItem.innerHTML = "Lectures per Week: " + courseArr[foundItem].lecNum;
+      }
+      else if(i==3)
+      {
+        // Add the item text
+        listItem.innerHTML = "Number of Projects: " + courseArr[foundItem].projects;
+      }
+      else if(i==4)
+      {
+        // Add the item text
+        listItem.innerHTML = "Total Reading Assignment Pages: " + (courseArr[foundItem].readWeek * 10);
+      }
+      else if(i==5)
+      {
+        // Add the item text
+        listItem.innerHTML = "Reading Assignments per Week: " + courseArr[foundItem].readWeek;
+      }
+
+      // Add listItem to the listElement
+      myList.appendChild(listItem);
+  }
 
 }
-});
+
+function openInfo(evt, name) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(name).style.display = "block";
+    evt.currentTarget.className += " active";
+}
