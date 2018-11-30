46d1439cd69efde9f82ac4ce613c2408402d937d
Bruce0042 <45467176+Bruce0042@users.noreply.github.com>
2018-11-30 10:08:57 -0800

    Delete LogInConfirmationPage.js

--- LogInConfirmationPage.js
+++
@@ -1,8 +0,0 @@
-
-function printLEmail(){
-  var emailAddress = localStorage.getItem("InputUserName");
-  document.getElementById('Clemail').innerHTML = "Email: " + emailAddress;
-}
-$(document ).ready(function() {
-  printLEmail();
-});
